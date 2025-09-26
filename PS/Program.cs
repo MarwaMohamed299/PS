cd49a7d83f5ce91042e8c7efd3b2d526adb1417d
MarwaMohamed299 <128231159+MarwaMohamed299@users.noreply.github.com>
2025-09-26 14:24:04 +0300

    Maximum Subarray

--- PS/Program.cs
+++ PS/Program.cs
@@ -910,6 +910,45 @@ Console.WriteLine("Hello, World!");
 //}
 #endregion
 
+#region Maximum Subarray [ Kadanes Algorithm]    TODO
+
+//Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+
+
+//Example 1:
+
+//Input: nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+//Output: 6
+//Explanation: The subarray[4, -1, 2, 1] has the largest sum 6.
+//Example 2:
+
+//Input: nums = [1]
+//Output: 1
+//Explanation: The subarray[1] has the largest sum 1.
+//Example 3:
+
+//Input: nums = [5, 4, -1, 7, 8]
+//Output: 23
+//Explanation: The subarray[5, 4, -1, 7, 8] has the largest sum 23.
+
+
+
+//public class Solution
+//{
+//    public int MaxSubArray(int[] nums)
+//    {
+//        int maxCount = nums[0];
+//        int currMax = nums[0];
+//        for (int i = 1; i < nums.Length; i++)
+//        {
+//            currMax = Math.Max(currMax + nums[i], nums[i]);
+//            maxCount = Math.Max(currMax, maxCount);
+//        }
+//        return maxCount;
+//    }
+//}
+
 #endregion
 
 #region Strings
