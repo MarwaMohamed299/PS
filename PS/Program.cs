664d7c6cf1f8f8ef8b6acfd2057f5ee7f169462f
MarwaMohamed299 <128231159+MarwaMohamed299@users.noreply.github.com>
2025-09-26 13:12:09 +0300

    How Many Numbers Are Smaller Than the Current Number

--- PS/Program.cs
+++ PS/Program.cs
@@ -257,7 +257,7 @@ Console.WriteLine("Hello, World!");
 
 #endregion
 
-#region Pivot Index
+#region Pivot Index TODO
 //Given an array of integers nums, calculate the pivot index of this array.
 
 //The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
@@ -594,7 +594,7 @@ Console.WriteLine("Hello, World!");
 //}
 #endregion
 
-#region  Shuffle the Array
+#region  Shuffle the Array TODO
 
 //Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
 
@@ -656,7 +656,7 @@ Console.WriteLine("Hello, World!");
 
 #endregion
 
-#region Number of Good Pairs
+#region Number of Good Pairs TODO
 
 //Given an array of integers nums, return the number of good pairs.
 
@@ -850,7 +850,66 @@ Console.WriteLine("Hello, World!");
 //}
 #endregion
 
-#region How Many Numbers Are Smaller Than the Current Number
+#region How Many Numbers Are Smaller Than the Current Number TODO
+
+//Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+
+//Return the answer in an array.
+
+
+
+//Example 1:
+
+//Input: nums = [8,1,2,2,3]
+//Output: [4,0,1,1,3]
+//Explanation: 
+//For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3). 
+//For nums[1]=1 does not exist any smaller number than it.
+//For nums[2]=2 there exist one smaller number than it (1). 
+//For nums[3]=2 there exist one smaller number than it (1). 
+//For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+
+//Return the answer in an array.
+
+
+
+//Example 1:
+
+//Input: nums = [8,1,2,2,3]
+//Output: [4,0,1,1,3]
+//Explanation: 
+//For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3). 
+//For nums[1]=1 does not exist any smaller number than it.
+//For nums[2]=2 there exist one smaller number than it (1). 
+//For nums[3]=2 there exist one smaller number than it (1). 
+//For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+
+
+//public class Solution
+//{
+//    public int[] SmallerNumbersThanCurrent(int[] nums)
+//    {
+//        var dict = new Dictionary<int, int>();
+//        var sortedNums = nums.OrderBy(x => x).ToArray();
+//        for (int i = 0; i < sortedNums.Length; i++)
+//        {
+//            if (!dict.ContainsKey(sortedNums[i]))
+//            {
+//                dict[sortedNums[i]] = i;
+//            }
+//        }
+//        int[] result = new int[nums.Length];
+//        for (int i = 0; i < nums.Length; i++)
+//        {
+//            result[i] = dict[nums[i]];
+//        }
+
+//        return result;
+
+//    }
+//}
+#endregion
+
 #endregion
 
 #region Strings
